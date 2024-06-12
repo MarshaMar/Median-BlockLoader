ee4dbae84fb9c8c4c5c1f887a2b10d215e2411a0
MarshaMar <marsalek.marek@seznam.cz>
2024-06-12 09:05:36 +0200

    Add files via upload

---
+++ BlockLoader-master/BlockLoader/PresentationLayer/BlockViewModel.cs
@@ -0,0 +1,18 @@
+﻿using BlockLoader.Utils;
+
+namespace BlockLoader.PresentationLayer
+{
+	public class BlockViewModel : NotifyPropertyChangedBase
+	{
+		public BlockViewModel(string code, int footage, string program)
+		{
+			Code = code;
+			Footage = footage;
+			Program = program;
+		}
+
+		public string Code { get; }
+		public int Footage { get; }
+		public string Program { get; }
+	}
+}
